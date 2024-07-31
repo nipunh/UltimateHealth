2f17c634ac7a43cb8e74ec90d6916d05f9bbdf67
nipunh <nipunhedaoo@gmail.com>
2024-07-31 10:42:08 -0400

    basic setup

--- frontend/src/components/ActivityOverview.tsx
+++ frontend/src/components/ActivityOverview.tsx
@@ -1,21 +1,133 @@
-import {View, Image} from 'react-native';
+import {View, Image, Text, StyleSheet} from 'react-native';
 import React from 'react';
+import { LineChart } from "react-native-gifted-charts";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { PRIMARY_COLOR } from '../helper/Theme';
 
 const ActivityOverview = () => {
+
+  const backgroundStyle = {
+    //backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: PRIMARY_COLOR,
+  };
+
+  const data = [
+    {
+      value: 2500,
+      frontColor: '#006DFF',
+      gradientColor: '#009FFF',
+      spacing: 6,
+      label: 'Jan',
+    },
+    {value: 2400, frontColor: '#3BE9DE', gradientColor: '#93FCF8'},
+
+    {
+      value: 3500,
+      frontColor: '#006DFF',
+      gradientColor: '#009FFF',
+      spacing: 6,
+      label: 'Feb',
+    },
+    {value: 3000, frontColor: '#3BE9DE', gradientColor: '#93FCF8'},
+
+    {
+      value: 4500,
+      frontColor: '#006DFF',
+      gradientColor: '#009FFF',
+      spacing: 6,
+      label: 'Mar',
+    },
+    {value: 4000, frontColor: '#3BE9DE', gradientColor: '#93FCF8'},
+
+    {
+      value: 5200,
+      frontColor: '#006DFF',
+      gradientColor: '#009FFF',
+      spacing: 6,
+      label: 'Apr',
+    },
+    {value: 4900, frontColor: '#3BE9DE', gradientColor: '#93FCF8'},
+
+    {
+      value: 3000,
+      frontColor: '#006DFF',
+      gradientColor: '#009FFF',
+      spacing: 6,
+      label: 'May',
+    },
+    {value: 2800, frontColor: '#3BE9DE', gradientColor: '#93FCF8'},
+  ];
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'white',
-      }}>
-      <Image
-        source={{
-          uri: 'https://i.ibb.co/T2WNVJZ/Whats-App-Image-2024-07-14-at-12-46-02-AM-1.jpg',
-        }}
-        style={{width: '95%', height: 600, resizeMode:"contain"}}
-      />
-    </View>
+    <SafeAreaView style={styles.scrollViewContentContainer}>
+    <Text style={{ fontSize: 18, fontFamily: '500'}}>
+        Monthly
+    </Text>
+    <Text style={{ fontSize: 12, fontFamily: '500'}}>
+        Date - Date Updated hourly
+    </Text>
+
+    <Text style={{ fontSize: 12, fontFamily: '500'}}>
+        Total Views
+    </Text>
+
+    <Text style={{ fontSize: 12, fontFamily: '500'}}>
+        Total Reads
+    </Text>
+
+          <LineChart 
+          data={data}
+          />
+  </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 6,
+  },
+  scrollViewContentContainer: {
+    paddingHorizontal: 16,
+    marginTop: 16,
+  },
+  button: {
+    height: 40,
+    width: 100,
+    borderRadius: 14,
+    marginHorizontal: 4,
+    marginVertical: 4,
+    padding: 6,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  labelStyle: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    textTransform: 'capitalize',
+  },
+  articleContainer: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: 0,
+  },
+  flatListContentContainer: {
+    paddingHorizontal: 16,
+    marginTop: 10,
+    paddingBottom: 120,
+  },
+  homePlusIconview: {
+    bottom: 100,
+    right: 25,
+    position: 'absolute',
+  },
+});
 
 export default ActivityOverview;
